c2c0563d2ac2fe5f1b877bf5148300400c9df654
Zach Waterfield <zlwaterfield@gmail.com>
2019-02-05 15:08:50 -0800

    Remove server

--- pages/edit.js
+++ pages/edit.js
@@ -1,14 +1,10 @@
-import React, { Component } from 'react';
-
-import Loading from '../components/Loading';
-
-import '../styles/main.scss';
-
-class Edit extends Component {
-
-  render() {
-    return <Loading />;
-  }
+const Edit = ({ id }) => (
+  <div>
+    <h1>{id}</h1>
+    <p>Welcome to the edit page for {id}!</p>
+  </div>
+)
+Edit.getInitialProps = async ({ query }) => {
+  return { id: query.id }
 }
-
-export default Edit;
+export default Edit
